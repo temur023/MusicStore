d5ec9b37b28547e5ea921c47eb9453063d899b74
te <fsdf@tes-MacBook-Air.local>
2026-02-13 10:22:27 +0500

    deploying the project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,5 @@ export default defineConfig({
     port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
     strictPort: true,
     host: "0.0.0.0",
-  },
-  server: {
-    port: 8080,
-    host: "0.0.0.0",
   }
 })
